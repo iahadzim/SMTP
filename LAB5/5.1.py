ee623e7b154400fb682a1cf04a9546fcf26c9f49
iman amirul <iahadzim@gmail.com>
2020-12-09 18:55:20 +0800

    UDP edited 5.1 with server side code

--- LAB5/5.1.py
+++ LAB5/5.1.py
@@ -17,3 +17,30 @@ print (data)
 
 s.close()
 
+
+#import socket
+
+#s = socket.socket()
+#print("Berjaya buat sokett")
+
+#port = 5005
+#ip = "192.168.0.117"
+
+#s.bind((ip, port))
+#print("Berjaya bind soket di port: " + str(port))
+
+#s.listen(5)
+#print("soket tengah menunggu client!")
+
+#message = b'Thank you'
+
+#while True:
+#       c, addr = s.accept()
+#        print("Dapat capaian dari: " + str(addr))
+#
+#        c.sendto(message, (ip, port))
+#        buffer = c.recv(1024)
+#        print(buffer)
+#c.close()
+
+
